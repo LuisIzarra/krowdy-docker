3eca374289618a919c2f645ba5e93bdaaac6b89f
LuisIzarra <lrodriguezizarra@gmail.com>
2022-09-05 19:11:00 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+// Here, add line code to call a Mongo or MySQL DB
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
