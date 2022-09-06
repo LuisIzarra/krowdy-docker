c10a923e47206ac2ac3d6d02cd56801e8be54d6e
LuisIzarra <lrodriguezizarra@gmail.com>
2022-09-05 20:45:07 -0500

    adds mysql dependencies, defines user and password, adds docker-compose command to run

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ docker build . -t luisizarra/backend:1.0
 ```
 docker run -p 8085:8085 -d luisizarra/backend:1.0
 ```
+
+### To run mysql and express app
+
+```
+docker-compose up --build --force-recreate --no-deps
+```
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 "use strict";
 
 const express = require("express");
-// Here, add line code to call a Mongo or MySQL DB
+var mysql = require("mysql");
 
 // Constants
 const PORT = 8085;
 const HOST = "0.0.0.0";
+
+var connection = mysql.createConnection({
+  host: "mysqldb",
+  user: "krowdyuser",
+  password: "krowdypassword",
+});
+
+connection.connect(function (err) {
+  console.log(">>>>>>>>>>>>>>>>>>>>> mysql connected");
+});
 
 // App
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.get("/registrar", (req, res) => {});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
