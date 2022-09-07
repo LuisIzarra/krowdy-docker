ace9accdba2f62d63712e10fb9c9d8abb77c973b
LuisIzarra <lrodriguezizarra@gmail.com>
2022-09-06 19:02:50 -0500

    fixs password and host error into server and docker-compose

--- README.md
+++ README.md
@@ -10,8 +10,14 @@ docker build . -t luisizarra/backend:1.0
 docker run -p 8085:8085 -d luisizarra/backend:1.0
 ```
 
-### To run mysql and express app
+### To run mysql and express app (without cache)
 
 ```
 docker-compose up --build --force-recreate --no-deps
+```
+
+### To run mysql and express app (with cache)
+
+```
+docker-compose up
 ```
--- server.js
+++ server.js
@@ -9,12 +9,30 @@ const HOST = "0.0.0.0";
 
 var connection = mysql.createConnection({
   host: "mysqldb",
-  user: "krowdyuser",
-  password: "krowdypassword",
+  user: "root",
+  password: "",
+  database: "krowdydb",
 });
 
 connection.connect(function (err) {
   console.log(">>>>>>>>>>>>>>>>>>>>> mysql connected");
+  if (err) throw err;
+  var sql = "CREATE TABLE usuarios (nombre VARCHAR(255))";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+    var user = { nombre: "Juan" };
+    var query = connection.query(
+      "INSERT INTO usuarios SET ?",
+      user,
+      function (err, result) {
+        if (err) throw err;
+        console.log("error", err);
+        console.log("result", result);
+      }
+    );
+    console.log(query.sql);
+  });
 });
 
 // App
